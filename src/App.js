65cacc4d7c7ed9c19e55032f39363f441142dab6
Paul Chesa <paulchesa1@gmail.com>
2020-12-21 16:49:11 +0300

    set up sheets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import config from './config'
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetId}/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${config.apiKey}`
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        let batchRowValues = data.valueRanges[0].values
+
+        const rows = []
+        for (let i = 1; i < batchRowValues.length; i++) {
+          let rowObject = {}
+          for (let j = 0; j < batchRowValues[++i].length; j++) {
+            rowObject[batchRowValues[0][j]] = batchRowValues[i][j]
+          }
+          rows.push(rowObject)
+        }
+
+        this.setState({ items: rows })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg fixed-top is-white is-dark-text">
+          <div className="navbar-brand h1 mb-0 text-large font-medium">
+            Online Retail Dashboard
+          </div>
+          <div className="navbar-nav ml-auto">
+            <div className="user-detail-section">
+              <span className="pr-2">Hi, Sean</span>
+              <span className="img-container">
+                <img
+                  src="https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png"
+                  className="rounded-circle"
+                  alt="user"
+                />
+              </span>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
+  }
 }
 
-export default App;
+App.propTypes = {}
+
+export default App
---
+++ src/config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    apikey: "AIzaSyC6q5LWF013iwhZ9GH7lfsZIGXg-vHVfZA",
+    spreadsheetId: "1sCgmzBIq2K9jUckLuYSWbDq4CuNUfdtuE6a5xI3I5Hw"
+
+}
