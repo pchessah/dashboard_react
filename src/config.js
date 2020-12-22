5ff17ba648c577edc568a35658ece198e38f9d07
Paul Chesa <paulchesa1@gmail.com>
2020-12-22 07:10:14 +0300

    changed config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    apikey: "AIzaSyC6q5LWF013iwhZ9GH7lfsZIGXg-vHVfZA",
+    apikey: "AIzaSyDeZ6LTVgljfs6db-CFoSz5kC5Z5NTdviU",
     spreadsheetId: "1sCgmzBIq2K9jUckLuYSWbDq4CuNUfdtuE6a5xI3I5Hw"
 
 }
